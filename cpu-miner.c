a1429e4196bb5ae37601e87b4cd1ca0f315dfad6
curvehash <info@curvehash.com>
2021-04-15 18:19:24 +0200

    removed wrong diff factor

--- cpu-miner.c
+++ cpu-miner.c
@@ -1849,7 +1849,6 @@ static void stratum_gen_work(struct stratum_ctx *sctx, struct work *work)
 		}
 
 		switch (opt_algo) {
-			case ALGO_CURVEHASH:	
 			case ALGO_DROP:
 			case ALGO_JHA:
 			case ALGO_SCRYPT:
